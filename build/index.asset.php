7c4beeb488054b61875cdcabfda22bc30094e025
pavelzolotin <pavel.zolotin@goat.digital>
2022-04-06 17:57:36 +0500

    managing meta fields using blocks

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => 'fd2891dbd18847d36df6695591c58a36');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '684b935ed53510ca13a441a758e83c62');
